61b4436ffb403df02040e3f4e91df41bd3f6397f
Mohamed Hatem <Mohamed.Hatem157@gmail.com>
2026-09-14 07:54:16 +0300

    Add project files.

---
+++ MVCDemoD06/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCDemoD06.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
